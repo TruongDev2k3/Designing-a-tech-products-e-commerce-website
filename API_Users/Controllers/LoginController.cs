0f64bd0d8b0f4ba31d4656900403fa99dd7414fb
TruongDev2k3 <nguyenvantruong05032003@gmail.com>
2024-09-23 08:00:42 +0700

    Add project files.

---
+++ API_Users/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using BTL_NguyenVanTruong_.BLL;
+using MODEL;
+using BTL_NguyenVanTruong_.DAL.Interfaces;
+using Microsoft.Extensions.Configuration;
+using BTL_NguyenVanTruong_.BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+
+namespace BTL_NguyenVanTruong_.API_User.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private IUserBussiness _userBusiness;
+        public LoginController(IUserBussiness userBusiness)
+        {
+            _userBusiness = userBusiness;           
+        }
+        //[AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] AuthenticateModel model)
+        {
+            var user = _userBusiness.Login(model.Username, model.Password);
+
+            if (user != null)
+            {
+                // Gán vai trò dựa trên thông tin người dùng (ví dụ: ADMIN hoặc USER)
+                var roles = new List<string> { user.Loai.ToString() };
+
+                // Tạo claims cho vai trò
+                var claims = roles.Select(role => new Claim(ClaimTypes.Role, role));
+
+                // Tạo identity và principal
+                var identity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+
+                // Gán principal cho người dùng
+                HttpContext.SignInAsync(principal);
+
+                return Ok(new { taikhoan = user.TenTaiKhoan, email = user.Email, Loai = user.Loai, token = user.Token });
+            }
+            else
+            {
+                return Unauthorized(new { Message = "Tên đăng nhập hoặc mật khẩu không đúng." });
+            }
+        }
+
+    }
+}
---
+++ BLL/UserBussiness.cs
@@ -0,0 +1,74 @@
+﻿using MODEL;
+using BTL_NguyenVanTruong_.DAL.Interfaces;
+using BTL_NguyenVanTruong_.BLL;
+using BTL_NguyenVanTruong_.BLL.Interfaces;
+using BTL_NguyenVanTruong_.DAL;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Sockets;
+using System.Security.Claims;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+namespace BTL_NguyenVanTruong_.BLL
+{
+    public partial class UserBussiness : IUserBussiness
+    {
+        public static IConfiguration _configuration { get; set; }
+        private readonly IUserRepository _res;
+        private string secret;
+
+        public UserBussiness(IUserRepository res, IConfiguration configuration)
+        {
+            _res = res;
+            secret = configuration["AppSettings:Secret"];
+        }
+        private string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            _configuration = builder.Build();
+            return _configuration.GetConnectionString("DefaultConnection");
+        }
+        public UserModel Login(string taikhoan, string matkhau)
+        {
+            var user = _res.Login(taikhoan, matkhau);
+            if (user == null)
+                return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            // Tạo danh sách claims
+            var claims = new List<Claim>
+             {
+                 new Claim(ClaimTypes.Name, user.TenTaiKhoan.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            // Thêm claim cho vai trò (admin hoặc user)
+            claims.Add(new Claim(ClaimTypes.Role, user.Loai.ToString()));
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            return user;
+        }
+
+
+        // Nếu không tìm thấy người dùng
+
+
+    }
+}
